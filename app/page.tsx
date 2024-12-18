e20f2393810780d52d72442ce9010a51cf81e059
freshwaterlemon <nguiyaksiong@gmail.com>
2024-12-18 18:06:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { CalendarDateRangePicker } from '@/components/date-range-picker';
-import { Tabs, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import { Tabs, TabsList, TabsContent, TabsTrigger } from '@/components/ui/tabs';
 import { OverviewTabContent } from '@/components/overview-tab-content';
+import { Skeleton } from '@/components/ui/skeleton';
 
 export default async function Home() {
 	// await new Promise(resolve => setTimeout(resolve, 3000));
@@ -34,6 +35,36 @@ export default async function Home() {
 						</TabsList>
 
 						<OverviewTabContent />
+						<TabsContent value="analytics" className="space-y-4">
+							<div className="flex flex-col space-y-3">
+								<Skeleton className="h-[125px] w-[250px] rounded-xl" />
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+								</div>
+							</div>
+						</TabsContent>
+						<TabsContent value="reports" className="space-y-4">
+							<div className="flex flex-col space-y-3">
+								<Skeleton className="h-[125px] w-[250px] rounded-xl" />
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+								</div>
+							</div>
+						</TabsContent>
+						<TabsContent
+							value="notifications"
+							className="space-y-4"
+						>
+							<div className="flex flex-col space-y-3">
+								<Skeleton className="h-[125px] w-[250px] rounded-xl" />
+								<div className="space-y-2">
+									<Skeleton className="h-4 w-[250px]" />
+									<Skeleton className="h-4 w-[200px]" />
+								</div>
+							</div>
+						</TabsContent>
 					</Tabs>
 				</div>
 			</div>
